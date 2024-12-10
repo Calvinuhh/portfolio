64f9e77e6cc84d7fafb8e72f586ca7b4583048b6
Calvin Ulis Howard Howard <Calvin.uhh@gmail.com>
2024-12-10 09:43:35 -0500

    Pequeña correccion

--- src/components/LanguagueButton/LanguagueButton.tsx
+++ src/components/LanguagueButton/LanguagueButton.tsx
@@ -2,17 +2,7 @@ import HeaderInterface from "../../interfaces/header.interface";
 import style from "./index.module.css";
 import { Link } from "react-router-dom";
 
-const LanguagueButton = (
-  props: Omit<
-    HeaderInterface,
-    | "seniority"
-    | "title_succes"
-    | "text_succes"
-    | "title_error"
-    | "text_error"
-    | "btn_accept"
-  >
-) => {
+const LanguagueButton = (props: Omit<HeaderInterface, "seniority">) => {
   return (
     <Link className={style.link} to={props.route}>
       {props.btn_text}
